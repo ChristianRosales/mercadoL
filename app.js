ccce16af10888bdeaebd8c91f719890c93a58e66
onix <christian.rosales9029@gmail.com>
2021-06-27 21:00:09 -0500

    se agrego flexbox y animacion

--- app.js
+++ app.js
@@ -12,4 +12,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+
 app.listen(port, () => console.log('Servidor corriendo en el puerto 3000'));
