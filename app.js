8926651b6b95279df15a8cda4cf7462e65ce0c6a
ChristianRosales <christian.rosales9029@gmail.com>
2021-07-01 12:18:05 -0500

    modificacion variable de entorno para heroku

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 const publicPath = path.resolve(__dirname, './public');
-
-app.use(express.static(publicPath) )
-
-
+app.use(express.static(publicPath) );
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
-})
-
+});
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
-})
+});
 app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
-})
+});
 
-app.listen(port, () => console.log('Servidor corriendo en el puerto 3000'));
+app.listen(PORT, () => console.log('Servidor corriendo en el puerto ' + PORT));
